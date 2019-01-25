de29e582a92066ddf573ed4d88933897f9f337d6
Joe Ganobsik <jganobsik@gmail.com>
2019-01-25 21:51:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     end
     
      post '/teams' do
-      @team_name = team.new(params[:super_hero])
+      @team = team.new(params[:super_hero])
 
       params[:team][:hero].each do |details|
         Hero.new(details)
@@ -19,3 +19,10 @@ class App < Sinatra::Base
       erb :team
     end
 end
+
+ @heros = params[:team][:heros]
+    @heros.each do |hero, item|
+      @hero_name << item[:name]
+      @hero_power << item[:power]
+      @hero_bio << item[:bio]
+    end
