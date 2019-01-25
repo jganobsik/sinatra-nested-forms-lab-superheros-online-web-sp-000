cfe744c94d5b8045efae70fe94e9b21fc4931e6e
Joe Ganobsik <jganobsik@gmail.com>
2019-01-25 22:05:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     erb :super_hero
   end
 
-  post '/teams' do
+  post '/team' do
     @team_name = params[:team][:name]
     @team_motto = params[:team][:motto]
     @hero_name = []
