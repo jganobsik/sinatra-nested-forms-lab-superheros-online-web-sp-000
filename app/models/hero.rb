20ef025e735076331006a2fb9ec823a4179bb7c9
Joe Ganobsik <jganobsik@gmail.com>
2019-01-25 21:46:30 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,11 +1,15 @@
 class Hero
   
   attr_accessor :name, :power, :bio 
-  
+  @@all = []
   def initialize(name, power, bio)
     @name =  name
     @power =  power
-    @bio = bio 
+    @bio = bio
+    @@all << self 
   end
   
+  def self.all
+    @@all
+  end
 end
